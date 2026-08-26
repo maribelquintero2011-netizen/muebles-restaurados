ebc001cae5a4f8ca62a2b25051c21a4d453d0d6d
Maribel <maribelquintero2011@gmail.com>
2026-08-26 19:55:39 +0200

    aumentar el tamaño del logo y quitar la palabra maravilleria

--- app/page.tsx
+++ app/page.tsx
@@ -293,7 +293,7 @@ export default function Home() {
       {/* ========================================== */}
       <header className="bg-white shadow-sm py-4 px-6 flex justify-between items-center sticky top-0 z-40">
         <button onClick={() => navigateTo('catalogo')} className="flex items-center gap-3 font-bold text-lg text-blue-900">
-           <img src="/logo.jpg" alt="Logo" className="w-10 h-10 object-contain rounded-full" />
+           <img src="/logo.jpg" alt="Logo" className="w-10  h-10 object-contain rounded-full" />
            MARAVILLERÍA STUDIO
         </button>
 
